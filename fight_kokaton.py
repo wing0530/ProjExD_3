97c0d253e2fd5e0d0c53fc7d2077b6afea64c795
tsubasa <c0a2417498@edu.teu.ac.jp>
2025-04-29 14:04:56 +0900

    練習２

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -159,19 +159,30 @@ def main():
                 beam = Beam(bird)            
         screen.blit(bg_img, [0, 0])
         
-        if bird.rct.colliderect(bomb.rct):
-            # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
-            bird.change_img(8, screen)
-            pg.display.update()
-            time.sleep(1)
-            return
+        # こうかとん衝突
+        if bomb != None:
+            if bird.rct.colliderect(bomb.rct):
+                # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
+                bird.change_img(8, screen)
+                pg.display.update()
+                time.sleep(1)
+                return
+        
+        # 爆弾破壊
+        if bomb != None:
+            if beam != None:
+                if beam.rct.colliderect(bomb.rct):
+                    beam = None
+                    bomb = None
+
 
         # アプデ
         key_lst = pg.key.get_pressed()
         bird.update(key_lst, screen)
         if beam != None:
-            beam.update(screen)   
-        bomb.update(screen)
+            beam.update(screen) 
+        if bomb != None:
+            bomb.update(screen)
         pg.display.update()
         tmr += 1
         clock.tick(50)
