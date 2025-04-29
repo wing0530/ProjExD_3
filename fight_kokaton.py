fd561090861033fd4408eac2b01a2c78cc53040f
tsubasa <c0a2417498@edu.teu.ac.jp>
2025-04-29 14:12:31 +0900

    練習4

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -164,6 +164,9 @@ def main():
             if bird.rct.colliderect(bomb.rct):
                 # ゲームオーバー時に，こうかとん画像を切り替え，1秒間表示させる
                 bird.change_img(8, screen)
+                fonto = pg.font.Font(None, 80)
+                txt = fonto.render("Game Over", True, (255, 0, 0))
+                screen.blit(txt, [WIDTH//2-150, HEIGHT//2])
                 pg.display.update()
                 time.sleep(1)
                 return
